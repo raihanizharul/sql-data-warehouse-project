ae2a46312016e9e632244f58999158e90763d177
raihan <raihanizharul7@gmail.com>
2025-11-01 23:35:01 +0700

    koreksi nama tabel dan tambah kode drop tabel jika tabel ada

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -6,9 +6,19 @@ DDL Script: Create Bronze Tables
 Script Purpose:
     This script creates tables in the 'bronze' schema, dropping existing tables
     if they already exist. 
+
     Run this script to re-define the DDL structure of 'bronze' Tables 
 
 */
+
+-- CRM --
+
+USE DWH_Enterprise;
+GO
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
 
 CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
@@ -22,7 +32,11 @@ CREATE TABLE bronze.crm_cust_info (
 );
 GO
 
-CREATE TABLE bronze.crm_prd_id(
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+
+CREATE TABLE bronze.crm_prd_info(
     prd_id INT,
     prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
@@ -32,6 +46,10 @@ CREATE TABLE bronze.crm_prd_id(
     prd_end_dt DATE
 
 );
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
 GO
 
 CREATE TABLE bronze.crm_sales_details(
@@ -47,11 +65,22 @@ CREATE TABLE bronze.crm_sales_details(
 );
 GO
 
+
+-- ERP --
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
 CREATE TABLE bronze.erp_cust_az12(
     cid NVARCHAR(50),
     bdate DATE,
     gen NVARCHAR(50)
 );
+GO
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
 GO
 
 CREATE TABLE bronze.erp_loc_a101(
@@ -60,9 +89,14 @@ CREATE TABLE bronze.erp_loc_a101(
 );
 GO
 
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
 CREATE TABLE bronze.erp_px_cat_g1v2(
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
     maintenance NVARCHAR(50)
 )
+
